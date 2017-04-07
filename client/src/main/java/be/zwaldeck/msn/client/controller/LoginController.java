52ab3e55a4a203be609858f7e7ef86cad54ee113
Wout Schoovaerts <schoovaertswout@live.be>
2017-04-07 17:54:32 +0200

    Quick Remeber me functionality

--- client/src/main/java/be/zwaldeck/msn/client/controller/LoginController.java
+++ client/src/main/java/be/zwaldeck/msn/client/controller/LoginController.java
@@ -1,8 +1,10 @@
 package be.zwaldeck.msn.client.controller;
 
+import be.zwaldeck.msn.client.MsnClient;
 import be.zwaldeck.msn.client.data.ApplicationData;
 import be.zwaldeck.msn.client.net.ServerConnection;
 import be.zwaldeck.msn.client.util.NavigationUtil;
+import be.zwaldeck.msn.client.util.PreferencesConstants;
 import be.zwaldeck.msn.common.messages.ServerMessage;
 import be.zwaldeck.msn.common.messages.ServerMessageType;
 import be.zwaldeck.msn.common.messages.data.LoginData;
@@ -16,14 +18,15 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import org.apache.commons.validator.routines.EmailValidator;
 
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 /**
  * @Author Wout Schoovaerts
  */
-//TODO remember me functionality
 public class LoginController extends GuiController {
 
     @FXML
@@ -38,6 +41,21 @@ public class LoginController extends GuiController {
     @FXML
     private Label errorLbl;
 
+    @Override
+    public void init(Stage stage) {
+        super.init(stage);
+        Preferences prefs = Preferences.userNodeForPackage(MsnClient.class);
+        String email = prefs.get(PreferencesConstants.LOGIN_EMAIL, "");
+        String password = prefs.get(PreferencesConstants.LOGIN_PASSWORD, "");
+        if(!email.isEmpty()) {
+            emailTxt.setText(email);
+        }
+
+        if(!password.isEmpty()) {
+            passwordTxt.setText(password);
+        }
+    }
+
     @FXML
     void onCreateAccount(ActionEvent event) throws IOException {
         NavigationUtil.openFxmlInSameScene(primaryStage, "/register.fxml", 220, 420);
@@ -48,6 +66,13 @@ public class LoginController extends GuiController {
         //todo loading overlay
         errorLbl.setVisible(false);
         if (!formErrors()) {
+
+            if(rememberMeCb.isSelected()) {
+                Preferences prefs = Preferences.userNodeForPackage(MsnClient.class);
+                prefs.put(PreferencesConstants.LOGIN_EMAIL, emailTxt.getText().trim());
+                prefs.put(PreferencesConstants.LOGIN_PASSWORD, passwordTxt.getText().trim());
+            }
+
             LoginData data = new LoginData(emailTxt.getText().trim(), passwordTxt.getText().trim());
 
             serverConnection.sendMessage(new ServerMessage(ServerMessageType.LOGIN, data));
---
+++ client/src/main/java/be/zwaldeck/msn/client/util/PreferencesConstants.java
@@ -0,0 +1,10 @@
+package be.zwaldeck.msn.client.util;
+
+/**
+ * @Author Wout Schoovaerts
+ */
+public class PreferencesConstants {
+
+    public static final String LOGIN_EMAIL = "LOGIN_EMAIL";
+    public static final String LOGIN_PASSWORD = "LOGIN_PASSWORD";
+}
